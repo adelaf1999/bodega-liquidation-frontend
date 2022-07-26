3936734062bdf920aeb6acd7e247103d9740a806
Adel Abou Fakher <adelwaboufakher@gmail.com>
2022-07-25 21:16:59 -0500

    Initial Landing page design

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {initializeHomePage}  from "../actions";
-
+import { Navbar, Form, FormControl, Offcanvas, Button} from "react-bootstrap";
 
 
 class Landing extends Component{
@@ -14,33 +14,105 @@ class Landing extends Component{
         
         super(props);
 
+        const width = window.innerWidth;
+
+        const height = window.innerHeight;
+
+
         this.state = {
             history,
-            params
+            params,
+            width,
+            height
         };
 
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
 
+
+    }
+
+
+    updateWindowDimensions() {
+
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
+
+    }
+
+
+    componentWillUnmount() {
+
+        window.removeEventListener('resize', this.updateWindowDimensions);
 
     }
 
 
     componentDidMount(){
 
+        this.updateWindowDimensions();
 
+        window.addEventListener('resize', this.updateWindowDimensions);
 
         this.props.initializeHomePage();
-
 
     }
 
 
 
+
     render(){
+
+        // show bars on the left if on mobile device or smaller screen
 
         return(
 
             <div>
-                <p>Welcome to the landing page!</p>
+
+                <Navbar style={{backgroundColor: '#000080'}}>
+
+                    <div>
+
+                        <Navbar.Brand
+                            onClick={() => {
+                                this.state.history.push("/");
+                            }}
+                            style={{
+                                color: "#fff",
+                                fontWeight: 'bold',
+                                fontSize: 18,
+                                marginLeft: '1rem'
+                            }}
+                        >
+                            Bodega Liquidation
+                        </Navbar.Brand>
+
+                    </div>
+
+
+                    <Form className="searchbar-container">
+
+                        <FormControl
+                            type="text"
+                            placeholder="Search Bodega Liquidation"
+                            className="mr-sm-2 searchbar"
+                            style={{
+                                marginRight: this.state.width / 5.5
+                            }}
+                        />
+
+                    </Form>
+
+
+
+
+
+                <div>
+
+                </div>
+
+
+                </Navbar>
+
+
             </div>
 
         );
